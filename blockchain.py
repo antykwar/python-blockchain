dadb6455fa7f8cbe4ab4722c81b240e50ab28a6d
Maxim Ezhov <ezhov.maxim@advcake.com>
2023-01-31 01:07:15 +0500

    Moved blockchain verifications to separate class

--- blockchain.py
+++ blockchain.py
@@ -2,9 +2,10 @@ import json
 from functools import reduce
 
 
-from hash_util import hash_string_256, hash_block
+from hash_util import hash_block
 from block import Block
 from transaction import Transaction
+from verification import Verification
 
 MINING_REWARD = 10
 MINING_SENDER = 'ABYSS'
@@ -13,6 +14,15 @@ BLOCKCHAIN_FILE = 'blockchain.dat'
 owner = 'Max'
 blockchain = []
 open_transactions = []
+
+
+def add_transaction(recipient, sender=owner, amount=1.0):
+    transaction = Transaction(sender, recipient, amount)
+    if Verification.verify_transaction(transaction, get_balance):
+        open_transactions.append(transaction)
+        save_data()
+        return True
+    return False
 
 
 def save_data():
@@ -59,33 +69,13 @@ def load_data():
         open_transactions = []
 
 
-def valid_proof(transactions, last_hash, proof):
-    guess = (str([tx.to_ordered_dict() for tx in transactions]) + str(last_hash) + str(proof)).encode()
-    guess_hash = hash_string_256(guess)
-    return guess_hash.startswith('00')
-
-
 def proof_of_work():
     last_block = blockchain[-1]
     last_hash = hash_block(last_block)
     proof = 0
-    while not valid_proof(open_transactions, last_hash, proof):
+    while not Verification.valid_proof(open_transactions, last_hash, proof):
         proof += 1
     return proof
-
-
-def verify_transaction(transaction):
-    sender_balance = get_balance(transaction.sender)
-    return sender_balance >= transaction.amount
-
-
-def add_transaction(recipient, sender=owner, amount=1.0):
-    transaction = Transaction(sender, recipient, amount)
-    if verify_transaction(transaction):
-        open_transactions.append(transaction)
-        save_data()
-        return True
-    return False
 
 
 def calculate_balance(participant, tx_type='sender'):
@@ -140,18 +130,6 @@ def get_last_blockchain_value():
         return None
 
 
-def verify_chain():
-    for index, block in enumerate(blockchain):
-        if index == 0:
-            continue
-        if block.previous_hash != hash_block(blockchain[index - 1]):
-            return False
-        if not valid_proof(block.transactions[:-1], block.previous_hash, block.proof):
-            return False
-
-    return True
-
-
 def read_transaction_value():
     recipient = input('Provide recipient, please: ')
     amount = float(input('Provide transaction sum, please: '))
@@ -182,10 +160,6 @@ def print_blockchain_elements():
         print(block)
 
 
-def verify_transactions():
-    return all([verify_transaction(tx) for tx in open_transactions])
-
-
 # -------------
 load_data()
 
@@ -206,7 +180,7 @@ while True:
     elif user_choice == '3':
         print_blockchain_elements()
     elif user_choice == '4':
-        if verify_transactions():
+        if Verification.verify_transactions(open_transactions, get_balance):
             print('All transactions are valid')
         else:
             print('There are invalid transactions')
@@ -215,7 +189,7 @@ while True:
     else:
         print('Invalid input! Please, provide value from the list.')
 
-    if not verify_chain():
+    if not Verification.verify_chain(blockchain):
         print('Corrupted blockchain!')
         break
 
--- verification.py
+++ verification.py
@@ -1,10 +1,29 @@
+from hash_util import hash_string_256, hash_block
+
+
 class Verification:
-    def verify_chain():
+    @classmethod
+    def valid_proof(cls, transactions, last_hash, proof):
+        guess = (str([tx.to_ordered_dict() for tx in transactions]) + str(last_hash) + str(proof)).encode()
+        guess_hash = hash_string_256(guess)
+        return guess_hash.startswith('00')
+
+    @classmethod
+    def verify_chain(cls, blockchain):
         for index, block in enumerate(blockchain):
             if index == 0:
                 continue
             if block.previous_hash != hash_block(blockchain[index - 1]):
                 return False
-            if not valid_proof(block.transactions[:-1], block.previous_hash, block.proof):
+            if not cls.valid_proof(block.transactions[:-1], block.previous_hash, block.proof):
                 return False
         return True
+
+    @classmethod
+    def verify_transaction(cls, transaction, get_balance_callback):
+        sender_balance = get_balance_callback(transaction.sender)
+        return sender_balance >= transaction.amount
+
+    @classmethod
+    def verify_transactions(cls, open_transactions, get_balance_callback):
+        return all([cls.verify_transaction(tx, get_balance_callback) for tx in open_transactions])
